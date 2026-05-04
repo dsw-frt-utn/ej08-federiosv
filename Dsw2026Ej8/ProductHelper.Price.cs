0d4c6a51b64751c008ad4535f95a5310e0feebed
Federico Rios <federico.riosvolentini@alu.frt.utn.edu.ar>
2026-05-04 19:20:44 -0300

    Resolucion de Problema 1

---
+++ Dsw2026Ej8/ProductHelper.Price.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        private string PriceFormatter(decimal price)
+        {
+            return price.ToString("C", CultureInfo.CurrentCulture);
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProductHelper pH = new ProductHelper();
+            string prueba1 = pH.ObtenerEtiquetaProducto(123456789, "Producto de prueba", 100);
+            Console.WriteLine(prueba1);
         }
     }
 }
